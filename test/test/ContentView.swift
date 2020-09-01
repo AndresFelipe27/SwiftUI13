a5ba2886ca7aa3e43166b4638df80df85593c696
AndresFelipe27 <anndresfee@hotmail.com>
2020-09-01 11:52:14 -0500

    added project test

---
+++ test/test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Felipe18 on 1/09/20.
+//  Copyright © 2020 Felipe18. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
